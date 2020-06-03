4ccab831d43bdec930a238165ccd3ccceec2f42f
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-02 23:00:26 -0400

    added comments for clarity

--- src/16_is_prime.py
+++ src/16_is_prime.py
@@ -13,18 +13,31 @@ To find  all the prime numbers less than or equal to a given integer `n` by Erat
 
 def main():
     num = int(input("Find prime numbers up to number: "))
-    # num = 30
+    # Set starting point to Smallest Prime number
     starting_num = 2
+
+    # Keep track of the index to advance loop
     index = 0
+
+    # Create a list from 2 - num
     list = [num for num in range(starting_num, num + 1)]
 
+    # If current prime * current prime is less than num
+    # continue the loop
     while starting_num * starting_num <= num:
+
+        # Slice just after the current prime to check remaining list
         for num in list[index+1:]:
             if num % starting_num == 0:
                 list.remove(num)
+
+        # Increase the index
         index += 1
+
+        # Set the starting point to the next number in the list
         starting_num = list[index]
 
+    # Final list of Prime numbers up to num
     print(list)
 
 
